d673240fc62121be8f9883ed43d14f537369350a
landauerm <max.landauer@ait.ac.at>
2020-06-26 14:43:23 +0200

    Add headers and license info

--- AECIDpg.py
+++ AECIDpg.py
@@ -1,3 +1,31 @@
+"""This is the main program of the AECID-parsergenerator (AECID-PG). The
+script analyzes log files and generates a parser model for the logdata-
+anomaly-miner. Configuration parameters are located in PGConfig.py
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+__authors__ = ["Markus Wurzenberger", "Max Landauer", "Wolfgang Hotwagner",
+               "Ernst Leierzopf", "Roman Fiedler", "Georg Hoeld"]
+__contact__ = "aecid@ait.ac.at"
+__copyright__ = "Copyright 2020, AIT Austrian Institute of Technology GmbH"
+__credits__ = ["Florian Skopik"]
+__date__ = "2020/06/26"
+__deprecated__ = False
+__email__ = "aecid@ait.ac.at"
+__license__ = "GPLv3"
+__maintainer__ = "Markus Wurzenberger"
+__status__ = "Production"
+__version__ = "0.0.1"
+
 from source import LogLine, Node, GlobalID
 import PGConfig
 from collections import Counter
--- PGConfig.py
+++ PGConfig.py
@@ -1,3 +1,16 @@
+"""This file holds the configuration parameters of the AECID-parsergenerator.
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
 input_file = 'data/in/audit.log'
 tree_file = 'data/out/tree.txt'
 parser_file = 'data/out/GeneratedParserModel.py'
